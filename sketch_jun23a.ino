3d4a650f0431938568aa3ca3288faa7e511a9505
Ranika p <renikarajeev890@gmail.com>
2026-06-24 19:59:00 +0530

    Add files via upload

---
+++ sketch_jun23a.ino
@@ -0,0 +1,12 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.print("shezin");
+}
+
+void loop() {}
